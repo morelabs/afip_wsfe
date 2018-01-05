993cacfdabafcb8b4da295948ad4b1c60844b776
Paco Moreno <pakerimus@gmail.com>
2018-01-04 23:59:25 -0300

    el login a WSAA se hace con savon. Mini tweaks y fixes.

--- lib/afip_wsfe/wsaa.rb
+++ lib/afip_wsfe/wsaa.rb
@@ -3,87 +3,92 @@ module AfipWsfe
   # valid key and signature that will last for a day.
   #
   class Wsaa
-    # Main method for authentication and authorization.
-    # When successful, produces the yaml file with auth data.
 
     def initialize(url=nil)
-      @url  = url || AfipWsfe::URLS[AfipWsfe.environment][:wsaa]
-      @tra  = nil
-      @cms  = nil
-      @req  = nil
+      AfipWsfe.environment ||= :test
+      @client = Savon.client(
+        wsdl:  AfipWsfe::URLS[AfipWsfe.environment][:wsaa],
+        namespaces: {
+          "xmlns:soapenv" => "http://schemas.xmlsoap.org/soap/envelope/",
+          "xmlns:ar" => "http://ar.gov.afip.dif.FEV1/"
+        },
+        log:       AfipWsfe.log?,
+        log_level: AfipWsfe.log_level || :debug,
+        ssl_verify_mode: :none,
+        read_timeout: 90,
+        open_timeout: 90,
+        headers: {
+          "Accept-Encoding" => "gzip, deflate",
+          "Connection" => "Keep-Alive"
+        },
+        convert_request_keys_to: :none
+      )
 
-      @token = nil
-      @sign  = nil
+      @cms  = nil
+      @performed = false
+      @response = nil
     end
 
-    def self.login
+    def login
+      raise "Archivo de llave privada no encontrado en #{ AfipWsfe.pkey }" unless File.exists?(AfipWsfe.pkey)
+      raise "Archivo certificado no encontrado en #{ AfipWsfe.cert }" unless File.exists?(AfipWsfe.cert)
       build_tra
       call_web_service
-      write_yaml
+      parse_response
+      status
+    end
+
+    def status
+      return false unless @performed
+      @response.success?
     end
 
     protected
 
-    # Builds the xml for the 'Ticket de Requerimiento de Acceso'
-    def self.build_tra
-      @now = (Time.now) - 120
-      @from = @now.strftime('%FT%T%:z')
-      @to   = (@now + ((12*60*60))).strftime('%FT%T%:z')
-      @id   = @now.strftime('%s')
+    def build_tra
+      now  = Time.zone.now
+      from = now.beginning_of_day.strftime('%FT%T%:z')
+      to   = now.end_of_day.strftime('%FT%T%:z')
+      id   = now.strftime('%s')
 
-      @tra = {
+      tra = {
         "header" => {
-          "uniqueId"       => @id,
-          "generationTime" => @from,
-          "expirationTime" => @to
+          "uniqueId"       => id,
+          "generationTime" => from,
+          "expirationTime" => to
         },
         "service" => "wsfe"
       }.to_xml(root: "loginTicketRequest")
 
-      build_cms
-      build_request
-    end
-
-    # Builds the CMS
-    def self.build_cms
-      @cms = `echo '#{ @tra }' |
+      @cms = `echo '#{ tra }' |
         #{ AfipWsfe.openssl_bin } cms -sign -in /dev/stdin -signer #{ AfipWsfe.cert } -inkey #{ AfipWsfe.pkey } -nodetach -outform der |
         #{ AfipWsfe.openssl_bin } base64 -e`
     end
 
-    # Builds the request
-    def self.build_request
-      builder = Nokogiri::XML::Builder.new do |xml|
-        xml.send("SOAP-ENV:Envelope", "xmlns:SOAP-ENV" => "http://schemas.xmlsoap.org/soap/envelope/", "xmlns:ns1" => "http://wsaa.view.sua.dvadac.desein.afip.gov") do
-          xml.send("SOAP-ENV:Body") do
-            xml.send("ns1:loginCms") do
-              xml.send("ns1:in0", @cms)
-            end
-          end
-        end
+    def call_web_service
+      body = { "ns1:in0" => @cms }
+      @response = client.call :login_cms, message: body
+      @performed = true
+    end
+
+    def parse_response
+      if status
+        response_hash = Hash.from_xml @response.body[:login_cms_response][:login_cms_return]
+        token = response_hash["loginTicketResponse"]["credentials"]["token"]
+        sign  = response_hash["loginTicketResponse"]["credentials"]["sign"]
+        write_yaml(token, sign)
       end
-      @req = builder.to_xml
     end
 
-    # Calls the WSAA
-    def self.call_web_service
-      response = `echo '#{ @req }' | curl -k -s -H 'Content-Type: application/soap+xml; action=""' -d @- #{ @url }`
-      response = CGI::unescapeHTML(response)
-      @token   = response.scan(/\<token\>(.+)\<\/token\>/).first.first
-      @sign    = response.scan(/\<sign\>(.+)\<\/sign\>/).first.first
-    end
-
-    # Writes the token and signature to a YAML file in the /tmp directory
-    def self.write_yaml(certs)
+    def write_yaml(token, sign)
       filename = "/tmp/bravo_#{ AfipWsfe.cuit }_#{ Time.zone.today.strftime('%Y_%m_%d') }.yml"
       content = {
-        token: @token,
-        sign: @sign
+        token: token,
+        sign: sign
       }
       File.open(filename, 'w') { |f|
         f.write content.to_yaml
       }
     end
-
   end
 end
